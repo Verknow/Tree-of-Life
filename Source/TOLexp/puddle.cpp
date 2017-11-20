18c7926e3ec2658b1ca895f2d2060f1ad98b2771
Vernon <grieger13@gmail.com>
2017-11-20 11:47:29 +1000

    Initial commit

---
+++ Source/TOLexp/puddle.cpp
@@ -0,0 +1,28 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "TOLexp.h"
+#include "puddle.h"
+
+
+// Sets default values
+Apuddle::Apuddle()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+}
+
+// Called when the game starts or when spawned
+void Apuddle::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void Apuddle::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
